396f1afa6520bf848f1e25adfac165474f9a0b7b
SepehrGanji <sepehrganji79@gmail.com>
2024-07-08 15:32:55 -0600

    add vga_buffer module

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+mod vga_buffer;
 use core::panic::PanicInfo;
 static HELLO: &[u8] = b"Hello World!";
 
@@ -16,7 +17,7 @@ pub extern "C" fn _start() -> ! {
     for (i, &byte) in HELLO.iter().enumerate() {
         unsafe {
             *vga_buffer.offset(i as isize * 2) = byte;
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xf;
         }
     }
 
